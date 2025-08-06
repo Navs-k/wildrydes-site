2e617ba3f1c8e3c33a37042ed0273ca3ecf3cd38
Navs-k <kumarknaveen314@gmail.com>
2025-08-05 21:56:37 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,10 +2,12 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_E8UpGsjpz', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '1aa9s8m4ooflfrttc6bci871fs', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        secret_hash: '14g6htgsv3ggdehhs8joj6b342ac7ds9unp8unj1vank64uip8cu'
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
